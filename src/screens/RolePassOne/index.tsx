b7a1ae952de815c896da23e691ef617e74e8cdad
Amorais19 <alicemoraisams@gmail.com>
2024-03-01 15:34:03 -0300

    mmm

--- src/screens/RolePassOne/index.tsx
+++ src/screens/RolePassOne/index.tsx
@@ -1,4 +1,4 @@
-import { ImageBackground, View, Text, Image } from "react-native";
+import { ImageBackground, View, Text, Image, ViewComponent } from "react-native";
 import { styleContainer } from "../../styles/globalstyles";
 import { styles } from "./styles"
 
@@ -8,14 +8,12 @@ export function RolePassOne() {
     return(
         <ImageBackground source={cogumelos} style={styleContainer.container}>
             <View style={styleContainer.container}>
-                <View>
+                <View style={styles.flex}>
                     <Text> Life Style </Text>
-                </View>
-                <View>
-                    <Image source={calendar}/>
-                </View>
-                <View>
-                    <Text style={styles.title}>Organize sua agenda, marque seus compromissos....</Text>
+                    <View style={styleContainer.container}>
+                        <Image source={calendar} style={styles.cont}/>
+                        <Text style={styles.words}>Organize sua agenda, marque seus compromissos....</Text>
+                    </View>
                 </View>
             </View>
         </ImageBackground>
--- src/screens/RolePassOne/styles.ts
+++ src/screens/RolePassOne/styles.ts
@@ -1,8 +1,16 @@
 import { StyleSheet} from 'react-native';
 
 export const styles = StyleSheet.create({
-    title: {
+    flex: {
+        flex: 0.7,
+        justifyContent: 'space-around',
+        alignItems: 'center',
+    },
+    words: {
         color: '#FFF',
         textAlign: 'center'
+    },
+    cont: {
+        
     },
   });
